39a89576da0cc30a855c4e2c7f11c4e89a846c12
Layer09 <119010896+Layer09@users.noreply.github.com>
2026-06-06 21:36:57 +0200

    Create relations.js

---
+++ js/relations.js
@@ -0,0 +1,99 @@
+let miis;
+let relations;
+
+async function init(){
+
+    miis =
+    await (await fetch("data/miis.json")).json();
+
+    relations =
+    await (await fetch("data/relations.json")).json();
+
+    buildTable();
+    buildSelectors();
+}
+
+function buildTable(){
+
+    const table =
+    document.getElementById("relationTable");
+
+    let html = "<tr><th></th>";
+
+    miis.forEach(m=>{
+
+        html += `<th>${m.prenom}</th>`;
+    });
+
+    html += "</tr>";
+
+    miis.forEach(m1=>{
+
+        html += `<tr><th>${m1.prenom}</th>`;
+
+        miis.forEach(m2=>{
+
+            let value = "-";
+
+            if(
+                relations[m1.id] &&
+                relations[m1.id][m2.id]
+            ){
+                value =
+                relations[m1.id][m2.id].niveau;
+            }
+
+            html += `<td>${value}</td>`;
+        });
+
+        html += "</tr>";
+    });
+
+    table.innerHTML = html;
+}
+
+function buildSelectors(){
+
+    const s1 =
+    document.getElementById("mii1");
+
+    const s2 =
+    document.getElementById("mii2");
+
+    miis.forEach(m=>{
+
+        s1.innerHTML +=
+        `<option value="${m.id}">
+        ${m.prenom}
+        </option>`;
+
+        s2.innerHTML +=
+        `<option value="${m.id}">
+        ${m.prenom}
+        </option>`;
+    });
+}
+
+document
+.getElementById("showRelation")
+.addEventListener("click", ()=>{
+
+    const id1 =
+    document.getElementById("mii1").value;
+
+    const id2 =
+    document.getElementById("mii2").value;
+
+    const relation =
+    relations[id1]?.[id2];
+
+    document.getElementById("result")
+    .innerHTML = relation
+    ? `
+      <h3>${relation.description}</h3>
+      <p>Niveau : ${relation.niveau}/100</p>
+      `
+    : "Aucune relation enregistrée";
+});
+
+init();
